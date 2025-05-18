d92e4c4588c4f9768c329982b92ab07492565d42
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-18 23:48:19 +0000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,6 @@
 provider "kubernetes" {
-  host     = "https://$(minikube ip):8443"
-  insecure = true  # Only for local development!
-  
-  client_certificate     = file("~/.minikube/profiles/minikube/client.crt")
-  client_key             = file("~/.minikube/profiles/minikube/client.key")
-  cluster_ca_certificate = file("~/.minikube/ca.crt")
+  host                   = "http://$(minikube ip):8443"
+  client_certificate     = file("/var/lib/jenkins/.minikube/profiles/minikube/client.crt")
+  client_key             = file("/var/lib/jenkins/.minikube/profiles/minikube/client.key") 
+  cluster_ca_certificate = file("/var/lib/jenkins/.minikube/ca.crt")
 }
