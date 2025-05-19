af3b457d5b18c56a49032619c8f1187415828c59
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-19 08:21:33 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "k8s_config_path" {
   description = "Path to kubeconfig file"
   default     = "~/.kube/config"
 }
+
+variable "minikube_ip" {
+  description = "IP address of Minikube cluster"
+  type        = string
+}
