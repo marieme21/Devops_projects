0fe630d84c9e0052996ce6c80d57ef2d9ce69794
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-24 19:08:35 +0000

    Update minikube.tf

--- terraform/minikube.tf
+++ terraform/minikube.tf
@@ -22,10 +22,10 @@ variable "cluster_ca_certificate" {
   type = string
 }
 
-provider "kubernetes" {
+/*provider "kubernetes" {
   host = var.host
 
   client_certificate     = base64decode(var.client_certificate)
   client_key             = base64decode(var.client_key)
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-}
+}*/
