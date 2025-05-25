cc9ce8eacdeaa525aa67dde040ff8b6ff4a86d43
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-25 12:53:32 +0000

    Update back-deployment.tf

--- terraform/back-deployment.tf
+++ terraform/back-deployment.tf
@@ -1,2 +1,67 @@
+resource "kubernetes_deployment" "backend" {
+  metadata {
+    name = "backend"
+    labels = {
+      app = "backend"
+    }
+  }
 
-back-deployment.yaml
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "backend"
+        }
+      }
+
+      spec {
+        container {
+          name  = "django"
+          image = "marieme21/projetfilrouge_backend:latest"
+          
+          port {
+            container_port = 8000
+          }
+
+          env {
+            name  = "DATABASE_URL"
+            value = "postgres://postgres:zou123@postgres-service/odc_db"
+          }
+
+          command = ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+        }
+
+        restart_policy = "Always"
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "backend" {
+  metadata {
+    name = "backend-service"
+  }
+
+  spec {
+    selector = {
+      app = "backend"
+    }
+
+    port {
+      name        = "http"
+      port        = 8000
+      target_port = 8000
+      node_port   = 30083
+    }
+
+    type = "NodePort"
+  }
+}
