5d899e08db49902d0f11f3550e8d7813d47f670e
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-25 10:47:38 +0000

    Update minikube.tf

--- terraform/minikube.tf
+++ terraform/minikube.tf
@@ -29,3 +29,54 @@ provider "kubernetes" {
   client_key             = base64decode(var.client_key)
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
+
+variable "minikube_ip" {
+  description = "Minikube cluster IP"
+  type        = string
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
