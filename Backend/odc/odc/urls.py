c65ac7cf28f59d15575e4b3275757e6b7da7794c
marieme21 <72065098+marieme21@users.noreply.github.com>
2025-05-27 22:34:17 +0000

    Update urls.py:  adding Prometheus metrics URL

--- Backend/odc/odc/urls.py
+++ Backend/odc/odc/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('profiles.urls'))
+    path('api/',include('profiles.urls')),
+    path('metrics/', exports.export_to_django_view)
 ]
